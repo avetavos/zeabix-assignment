2440e4d72939c3f4317fe3c6d97429e14389c4d5
Aveta Vos <avetavos@hotmail.com>
2025-09-25 12:51:59 +0700

    feat: initialize orders service with basic structure and functionality
    - Added package.json for orders service with dependencies and scripts.
    - Created AppController and AppService for handling basic requests.
    - Implemented OrdersModule, OrdersController, and OrdersService for order management.
    - Added DTOs for creating and updating orders.
    - Set up configuration management with ConfigModule.
    - Included Jest tests for controllers and services.
    - Established basic e2e testing for the application.
    - Configured TypeScript settings for the project.

---
+++ orders-service/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from './orders/orders.module';
+import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    OrdersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
